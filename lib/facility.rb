8a5b0f05e9697b9ef3d8c07d202e337a3c668365
Victor Sanchez <vsanantone@gmail.com>
2023-08-31 17:47:38 -0500

    fix: the readable attributes of name, address, phone and services on lines 5-8

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,9 +2,9 @@ class Facility
   attr_reader :name, :address, :phone, :services
 
   def initialize(data)#bug 2 wrong format receiving a hash with multiple data inside. Only one argumen
-    @name = name
-    @address = address
-    @phone = phone
+    @name = data[:name]#bug 3
+    @address = data[:address]
+    @phone = data[:phone]
     @services = []
   end
 
