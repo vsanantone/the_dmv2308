cbd87846cfb06b8e8cf3ff5576ba84350ea3f022
Victor Sanchez <vsanantone@gmail.com>
2023-09-04 16:35:33 -0500

    feat:add testing for dmv written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 16 )
+    @registrant_3 = Registrant.new('Tucker', 15 )
   end
 
   describe '#initialize' do
@@ -70,7 +74,6 @@ RSpec.describe Facility do
 
   describe '#collected_fees'do
     it 'collects car registration fees' do
-      # require 'pry'; binding.pry
       expect(@facility_1.register_vehicle(@cruz)).to eq([@cruz])
       expect(@facility_1.registered_vehicles).to eq([@cruz])
       expect(@facility_1.rcollected_fees(@cruz)).to eq(100)
@@ -86,14 +89,41 @@ RSpec.describe Facility do
       expect(@bolt.plate_type).to eq(:ev)
       expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
       expect(@facility_1.rcollected_fees(@bolt)).to eq(325) 
+    end
 
+    it ' registeres vehicles in the 2nd facility' do
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.services).to eq([])
-      # expect(@facility_2.register_vehicle(vehicle)).to eq(nil)
-      expect(@facility_2.register_vehicle(@bolt)).to eq([@bolt])
+      expect(@facility_2.collected_fees).to eq(0) 
+      @facility_2.register_vehicle(@bolt)#interaction pattern says nil
       expect(@facility_2.registered_vehicles).to eq([@bolt])
-      expect(@facility_1.rcollected_fees(@bolt)).to eq(200) 
+      expect(@facility_2.rcollected_fees(@bolt)).to eq(200) 
+    end
+  end
 
+  describe "written test" do
+    it "gives exam to registrant" do
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_1.permit?).to eq(true)
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      @facility_1.add_service('Written Test')
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+
+      expect(@registrant_2.age).to eq(16)
+      expect(@registrant_2.permit?).to eq(false)
+      expect(@facility_1.administer_written_test(registrant_2)).to eq(false)
+      @registrant_2.earn_permit
+      expect(@facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      
+      expect(@registrant_3.age).to eq(15)
+      expect(@registrant_3.permit?).to eq(false)
+      expect(@facility_1.administer_written_test(registrant_3)).to eq (false)
+      @registrant_3.earn_permit
+      expect(@facility_1.administer_written_test(registrant_3)).to eq (false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 end
