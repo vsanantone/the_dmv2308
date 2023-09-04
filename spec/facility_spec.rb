7e984bdb0c240cb02584d2bf0dfe2bf5e367f2d8
Victor Sanchez <vsanantone@gmail.com>
2023-09-04 17:10:43 -0500

    test: add testing for the road test in facility spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -126,4 +126,25 @@ RSpec.describe Facility do
       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
+
+  describe "Road Test" do
+    it "Road Test to registrant" do 
+      @facility_1.add_service('Written Test')
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(registrant_2)
+      @facility_1.administer_written_test(registrant_1)
+      
+      expect(@facility_1.administer_road_test(registrant_3)).to eq (false)
+      @registrant_3.earn_permit
+      expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
+      expect(@facility_1.administer_road_test(registrant_1)).to eq(false)
+      expect(@facility_1.add_service('Road Test')).to eq(["Written Test", "Road Test"])
+      expect(@facility_1.administer_road_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+      expect(@facility_1.administer_road_test(registrant_2)).to eq (true)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
+  end
 end
